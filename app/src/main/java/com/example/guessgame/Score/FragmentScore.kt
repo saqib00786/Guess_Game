ec427974a6ae5a09e1b7995ffc99608274cba3fe
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2020-12-30 20:08:16 +0500

    initial commit

---
+++ app/src/main/java/com/example/guessgame/Score/FragmentScore.kt
@@ -0,0 +1,14 @@
+package com.example.guessgame.Score
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.guessgame.R
+
+class FragmentScore : Fragment(R.layout.fragment_score) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/guessgame/Title/FragmentTitle.kt
@@ -0,0 +1,15 @@
+package com.example.guessgame.Title
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.guessgame.R
+
+
+class FragmentTitle : Fragment(R.layout.fragment_title) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
