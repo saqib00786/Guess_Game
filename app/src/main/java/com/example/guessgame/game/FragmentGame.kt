f62c3ff3f3b3f2116e4ccfefedfd05dfa3fc57cc
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2021-01-03 15:15:55 +0500

    initial commit

--- app/src/main/java/com/example/guessgame/game/FragmentGame.kt
+++ app/src/main/java/com/example/guessgame/game/FragmentGame.kt
@@ -1,16 +1,19 @@
 package com.example.guessgame.game
 
+import android.database.DatabaseUtils
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.guessgame.R
 import com.example.guessgame.databinding.FragmentGameBinding
 
-class FragmentGame : Fragment(R.layout.fragment_game) {
+class FragmentGame : Fragment() {
 
     private var mBinding: FragmentGameBinding? = null
     private val binding get() = mBinding!!
@@ -32,33 +35,35 @@ class FragmentGame : Fragment(R.layout.fragment_game) {
 
         mBinding?.nextBtnIDGame?.setOnClickListener {
             model.onCorrect()
-            updateWordText()
-            updateScoreText()
         }
         mBinding?.skipBtnIDGame?.setOnClickListener {
             model.onSkip()
-            updateWordText()
-            updateScoreText()
         }
+        model.mScore.observe(viewLifecycleOwner, {
+            mBinding?.currentScoreIDGame?.text = "Score: ${it}"
+        })
 
-        updateScoreText()
-        updateWordText()
+        model.mWord.observe(viewLifecycleOwner,{
+            mBinding?.wordGame?.text = "\"${it}\""
+        })
 
+        model.currentTime.observe(viewLifecycleOwner,{
+            mBinding?.timeCounterIDGame?.text = DateUtils.formatElapsedTime(it)
+        })
+
+        model.onGameFinished.observe(viewLifecycleOwner,{
+            if(it){
+                gameFinished()
+                model.onCompeleteGameFinished()
+            }
+        })
     }
 
 
     private fun gameFinished() {
-        val score = model.mScore.toString()
-        val action = FragmentGameDirections.actionFragmentGameToFragmentScore(score)
+       // val score = model.mScore.toString()
+        val action = FragmentGameDirections.actionFragmentGameToFragmentScore((model.mScore.value ?: 0).toString())
         findNavController().navigate(action)
-    }
-
-    private fun updateWordText() {
-        mBinding?.wordGame?.text = "\"${model.mWord}\""
-    }
-
-    private fun updateScoreText() {
-        mBinding?.currentScoreIDGame?.text = "Score: ${model.mScore}"
     }
 
 }
--- app/src/main/java/com/example/guessgame/game/GameViewModel.kt
+++ app/src/main/java/com/example/guessgame/game/GameViewModel.kt
@@ -1,17 +1,65 @@
 package com.example.guessgame.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    var mWord = ""
-    var mScore = 0
-    lateinit var mWordList: MutableList<String>
+
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 60000L
+    }
+
+    private val counDownTimer: CountDownTimer
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    private val _mWord = MutableLiveData<String>()
+    val mWord: LiveData<String>
+        get() = _mWord
+
+    // todo: restriction for mScore not access out of viewModel
+    private val _mScore = MutableLiveData<Int>()
+    val mScore: LiveData<Int>
+        get() = _mScore
+
+    // todo: for EVENT game finish code
+    private val _onGameFinished = MutableLiveData<Boolean>()
+    val onGameFinished: LiveData<Boolean>
+        get() = _onGameFinished
+
+    private lateinit var mWordList: MutableList<String>
 
     init {
         Log.i("GameViewModel", "GameViewModel created!")
+        _mScore.value = 0
+        _mWord.value = ""
+        _onGameFinished.value = false
         resetList()
         nextWord()
+        counDownTimer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = (millisUntilFinished.div(ONE_SECOND))
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _onGameFinished.value = true
+            }
+        }.start()
+
     }
 
 
@@ -47,10 +95,9 @@ class GameViewModel : ViewModel() {
 
     private fun nextWord() {
         if (mWordList.isEmpty()) {
-           // gameFinished()
-        } else {
-            mWord = mWordList.removeAt(0)
+            resetList()
         }
+        _mWord.value = mWordList.removeAt(0)
     }
 
     fun onSkip() {
@@ -58,12 +105,18 @@ class GameViewModel : ViewModel() {
     }
 
     fun onCorrect() {
-        mScore++
+        _mScore.value = mScore.value?.plus(1)
         nextWord()
+    }
+
+    fun onCompeleteGameFinished() {
+        _onGameFinished.value = false
+
     }
 
     override fun onCleared() {
         super.onCleared()
+        counDownTimer.cancel()
         Log.i("GameViewModel", "GameViewModel destroyed!")
     }
 }
